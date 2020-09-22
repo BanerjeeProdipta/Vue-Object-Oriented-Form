86aa61a7e4e6e748bde36f71d0d746b251856294
BanerjeeProdipta <prodiptaj@live.com>
2020-09-23 00:29:49 +0600

    With easy one

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,78 +1,212 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-const { default: Axios } = require('axios');
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 class Errors {
+    /**
+     * Create a new Errors instance.
+     */
     constructor() {
         this.errors = {};
     }
 
+
+    /**
+     * Determine if an errors exists for the given field.
+     *
+     * @param {string} field
+     */
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
+
+
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
+
+
+    /**
+     * Retrieve the error message for a field.
+     *
+     * @param {string} field
+     */
     get(field) {
         if (this.errors[field]) {
             return this.errors[field][0];
         }
     }
+
+
+    /**
+     * Record the new errors.
+     *
+     * @param {object} errors
+     */
     record(errors) {
         this.errors = errors;
     }
 
-    clear(field) {
-        delete this.errors[field];
-    }
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
 
-    any() {
-        return Object.keys(this.errors).length > 0;
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {string|null} field
+     */
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
+
+            return;
+        }
+
+        this.errors = {};
     }
 }
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        name: "",
-        description: "",
-        errors: new Errors()
-    },
-    methods: {
-        onSubmit() {
-            axios.post('/projects', this.$data)
-                .then(this.onSuccess)
-                .catch(error => this.errors.record(error.response.data.errors))
-        },
-        onSuccess(data) {
-            alert(data.message); // temporary
 
-            this.name = ''
-            this.description = '';
+class Form {
+    /**
+     * Create a new Form instance.
+     *
+     * @param {object} data
+     */
+    constructor(data) {
+        this.originalData = data;
+
+        for (let field in data) {
+            this[field] = data[field];
         }
 
+        this.errors = new Errors();
+    }
+
+
+    /**
+     * Fetch all relevant data for the form.
+     */
+    data() {
+        let data = {};
+
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+
+        return data;
+    }
+
+
+    /**
+     * Reset the form fields.
+     */
+    reset() {
+        for (let field in this.originalData) {
+            this[field] = '';
+        }
+
+        this.errors.clear();
+    }
+
+
+    /**
+     * Send a POST request to the given URL.
+     * .
+     * @param {string} url
+     */
+    post(url) {
+        return this.submit('post', url);
+    }
+
+
+    /**
+     * Send a PUT request to the given URL.
+     * .
+     * @param {string} url
+     */
+    put(url) {
+        return this.submit('put', url);
+    }
+
+
+    /**
+     * Send a PATCH request to the given URL.
+     * .
+     * @param {string} url
+     */
+    patch(url) {
+        return this.submit('patch', url);
+    }
+
+
+    /**
+     * Send a DELETE request to the given URL.
+     * .
+     * @param {string} url
+     */
+    delete(url) {
+        return this.submit('delete', url);
+    }
+
+
+    /**
+     * Submit the form.
+     *
+     * @param {string} requestType
+     * @param {string} url
+     */
+    submit(requestType, url) {
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    this.onSuccess(response.data);
+
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    this.onFail(error.response.data);
+
+                    reject(error.response.data);
+                });
+        });
+    }
+
+
+    /**
+     * Handle a successful form submission.
+     *
+     * @param {object} data
+     */
+    onSuccess(data) {
+        alert(data.message); // temporary
+
+        this.reset();
+    }
+
+
+    /**
+     * Handle a failed form submission.
+     *
+     * @param {object} errors
+     */
+    onFail(errors) {
+        this.errors.record(errors);
+    }
+}
+
+
+new Vue({
+    el: '#app',
+
+    data: {
+        form: new Form({
+            name: '',
+            description: ''
+        })
+    },
+
+    methods: {
+        onSubmit() {
+            this.form.post('/projects')
+                .then(response => alert('Wahoo!'));
+        }
     }
 });
---
+++ resources/js/easy.js
@@ -0,0 +1,55 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+class Errors {
+    constructor() {
+        this.errors = {};
+    }
+
+    get(field) {
+        if (this.errors[field]) {
+            return this.errors[field][0];
+        }
+    }
+    record(errors) {
+        this.errors = errors;
+    }
+
+    clear(field) {
+        delete this.errors[field];
+    }
+    has(field) {
+        return this.errors.hasOwnProperty(field);
+    }
+
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
+}
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        name: "",
+        description: "",
+        errors: new Errors()
+    },
+    methods: {
+        onSubmit() {
+            axios.post('/projects', this.$data)
+                .then(this.onSuccess)
+                .catch(error => this.errors.record(error.response.data.errors))
+        },
+        onSuccess(data) {
+            alert(data.message); // temporary
+
+            this.name = ''
+            this.description = '';
+        }
+
+    }
+});
